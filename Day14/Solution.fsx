9dfc46f2a14de7fcd7efb027f46f848045d175cc
Gustav Öhman <se-gusohma-01@SE-DEVPC1079>
2023-12-14 10:54:47 +0100

    Day 14

--- Day14/Solution.fsx
+++ Day14/Solution.fsx
@@ -22,25 +22,87 @@ let readFile fileName =
     with
         ex -> Error $"Could not read file '%s{fileName}': %s{ex.Message}" 
 
-let part1 (input: char array2d) =
-    let height = Array2D.length1 input 
-    seq{0..(height - 1)}
-    |>> (fun row ->
+let tiltNorth (input: char array2d) =
+    let length = Array2D.length1 input 
+    seq{0..(length - 1)}
+    |> Seq.iter (fun row ->
         let mutable col = input[*,row] |> String.ofArray
         while String.isSubString ".O" col do
             col <- Regex.Replace(col,"(\.+)(O+)","$2$1",RegexOptions.RightToLeft)
-        //Console.WriteLine $"changed {col} to {newCol}"
         col
+        |> Seq.iteri (fun i c -> input[i, row] <- c)
     )
-    |> Seq.sumBy ( fun col ->
+    input
+
+let tiltSouth (input: char array2d) =
+    let length = Array2D.length1 input 
+    seq{0..(length - 1)}
+    |> Seq.iter (fun row ->
+        let mutable col = input[*,row] |> String.ofArray
+        while String.isSubString "O." col do
+            col <- Regex.Replace(col,"(O+)(\.+)","$2$1")
         col
-        |> Seq.mapi (fun i c -> if c = 'O' then height - i else 0)
-        |> Seq.sum
-        )
+        |> Seq.iteri (fun i c -> input[i, row] <- c)
+    )
+    input
+
+let tiltWest (input: char array2d) =
+    let length = Array2D.length2 input 
+    seq{0..(length - 1)}
+    |> Seq.iter (fun row ->
+        let mutable col = input[row,*] |> String.ofArray
+        while String.isSubString ".O" col do
+            col <- Regex.Replace(col,"(\.+)(O+)","$2$1",RegexOptions.RightToLeft)
+        col
+        |> Seq.iteri (fun i c -> input[row,i] <- c)
+    )
+    input
+
+let tiltEast (input: char array2d) =
+    let length = Array2D.length2 input 
+    seq{0..(length - 1)}
+    |> Seq.iter (fun row ->
+        let mutable col = input[row,*] |> String.ofArray
+        while String.isSubString "O." col do
+            col <- Regex.Replace(col,"(O+)(\.+)","$2$1")
+        col
+        |> Seq.iteri (fun i c -> input[row,i] <- c)
+    )
+    input
+
+let spinCycle (input: char array2d) =
+    input
+    |> tiltNorth
+    |> tiltWest
+    |> tiltSouth
+    |> tiltEast
+
+let load  (input: char array2d) =
+    let height = Array2D.length1 input
+    input
+    |> Array2D.mapi (fun col row c -> if c = 'O' then height - col else 0)
+    |> Seq.cast<int>
+    |> Seq.sum
+let part1 (input: char array2d) =
+    input
+    |> Array2D.copy
+    |> tiltNorth
+    |> load
     |> sprintf "%i"
 
-let part2 input = 
-    "todo"
+let part2 (input: char array2d) = 
+    seq{1..1000}
+    |> Seq.fold 
+        (fun acc i ->
+            let newMap = spinCycle acc
+            Console.WriteLine $"{i} cycles, load {load newMap}"
+            newMap
+        )
+        input
+    |> load
+    |> sprintf "%i"
+
+    //TODO: Programatically find sequence lenght (14), calculate modulus (0) and pick correct value (the last one)
 
 module Tests =
     let private tests = 
